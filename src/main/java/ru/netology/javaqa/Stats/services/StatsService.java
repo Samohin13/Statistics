470d137e0dcac1f21033461b73858aee2e4d003d
Samohin Sergei <Bravochief21@gmail.com>
2024-07-18 19:42:17 +0500

    first commit

--- src/main/java/ru/netology/javaqa/Stats/services/StatsService.java
+++ src/main/java/ru/netology/javaqa/Stats/services/StatsService.java
@@ -3,7 +3,7 @@ package ru.netology.javaqa.Stats.services;
 
 public class StatsService {
     // сумму всех продаж;
-    public int sumYear(long[] sales) {
+    public long sumYear(long[] sales) {
 
         long salesAmount = 0;
 
@@ -11,109 +11,77 @@ public class StatsService {
             salesAmount = (salesAmount + sales[month]);
 
         }
-        return (int) salesAmount;
+        return  salesAmount;
     }
 
     //среднюю сумму продаж в месяц;
-    public int averageAmount(long[] sales) {
+    public long averageAmount(long[] sales) {
         long averageSale = sumYear(sales) / 12; // Средняя сумма прожаж
 
-        return (int) averageSale;
+        return  averageSale;
 
     }
 
     //номер месяца, в котором был пик продаж, то есть осуществлены
     // продажи на максимальную сумму*
-    public int maxSalesMonth(long[] sales) {
+    public long maxSalesMonth(long[] sales) {
         int maxMonth = 0;
 
-        for (int i = 0; i < sales.length; i++)
+        for (int i = 0; i < sales.length; i++) {
             if (sales[i] >= sales[maxMonth]) { // max месяц продаж
                 maxMonth = i;
             }
+        }
         return maxMonth + 1;
     }
 
     //номер месяца, в котором был минимум продаж,
     // то есть осуществлены продажи на минимальную сумму*;
-    public int minSalesMonth(long[] sales) {
-        int minMonth = 0;
+    public long minSalesMonth(long[] sales) {
+       int minMonth = 0;
 
-        for (int i = 0; i < sales.length; i++)
+        for (int i = 0; i < sales.length; i++) {
             if (sales[i] <= sales[minMonth]) {  // min месяц продаж
                 minMonth = i;
             }
+        }
         return minMonth + 1;
 
     }
 
     //количество месяцев,
     // в которых продажи были ниже среднего (см. п.2);
-    public int numMonthMinAmount(long[] sales) {
+    public  long numMonthMinAmount(long[] sales) {
 
-        int numMonMinAmount = averageAmount(sales);
+        long minAmount = averageAmount(sales);
 
-        int sumMonths = 0;
+         long  monthsCount = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] > numMonMinAmount) {
-                sumMonths++;
+            if (sales[i] > minAmount) {
+                monthsCount++;
             }
         }
-        return sumMonths;
+        return  monthsCount;
     }
 
     //количество месяцев, в которых продажи были выше среднего
-    public int numMonthMaxAmount(long[] sales) {
+    public long numMonthMaxAmount(long[] sales) {
 
-        int numMonMaxAmount = averageAmount(sales);
+        long maxAmount = averageAmount(sales);
 
-        int sumMonths = 0;
+        long  monthsCount = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < numMonMaxAmount) {
-                sumMonths++;
+            if (sales[i] < maxAmount) {
+                monthsCount++;
             }
         }
-        return sumMonths;
+        return  monthsCount;
 
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src/test/java/ru/netology/javaqa/Stats/services/StatsServiceTest.java
+++ src/test/java/ru/netology/javaqa/Stats/services/StatsServiceTest.java
@@ -6,80 +6,90 @@ import org.junit.jupiter.api.Test;
 
 public class StatsServiceTest {
     @Test
-
+// сумму всех продаж;
     public void summaYear() {
         StatsService service = new StatsService();
 
         long[] sum = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        int expectedSum = 180;
-        int actualSum = service.sumYear(sum);
+        long expectedSum = 180;
+        long actualSum = service.sumYear(sum);
 
         Assertions.assertEquals(expectedSum, actualSum);
 
     }
 
     @Test
-
+    //среднюю сумму продаж в месяц;
     public void averageAmountTest() {
         StatsService service = new StatsService();
 
         long[] sum = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        int expectedAverage = 15;
-        int actualAverage = service.averageAmount(sum);
+        long expectedAverage = 15;
+        long actualAverage = service.averageAmount(sum);
 
         Assertions.assertEquals(expectedAverage, actualAverage);
 
     }
 
     @Test
+
+    //номер месяца, в котором был пик продаж, то есть осуществлены
+    // продажи на максимальную сумму*
     public void maxSalesMonthTest() {
         StatsService service = new StatsService();
 
         long[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        int expectedMonth = 8;
-        int actualmonth = service.maxSalesMonth(month);
+        long expectedMonth = 8;
+        long actualmonth = service.maxSalesMonth(month);
 
         Assertions.assertEquals(expectedMonth, actualmonth);
 
     }
 
     @Test
+
+    //номер месяца, в котором был минимум продаж,
+    // то есть осуществлены продажи на минимальную сумму*;
     public void minSalesMonthTest() {
         StatsService service = new StatsService();
 
         long[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        int expectedMonth = 9;
-        int actualMonth = service.minSalesMonth(month);
+        long expectedMonth = 9;
+        long actualMonth = service.minSalesMonth(month);
 
         Assertions.assertEquals(expectedMonth, actualMonth);
 
     }
 
     @Test
+
+    //количество месяцев,
+    // в которых продажи были ниже среднего (см. п.2);
     public void numMonthMinAmountTest() {
         StatsService service = new StatsService();
 
         long[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMinAmount = 5;
-        int actualMinAmount = service.numMonthMinAmount(month);
+        long actualMinAmount = service.numMonthMinAmount(month);
 
         Assertions.assertEquals(expectedMinAmount, actualMinAmount);
 
     }
 
     @Test
+    //количество месяцев, в которых продажи были выше среднего
     public void numMonthMaxAmountTest() {
         StatsService service = new StatsService();
 
         long[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMaxAmount = 5;
-        int actualMaxAmount = service.numMonthMaxAmount(month);
+        long actualMaxAmount = service.numMonthMaxAmount(month);
 
         Assertions.assertEquals(expectedMaxAmount, actualMaxAmount);
 
